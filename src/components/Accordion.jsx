f2ddc9092c167fa8e96ae43c29b98a4cea8ce9a9
John Gustafson <John.Gustafson1@Marist.edu>
2021-02-19 16:08:56 -0500

    Finished Accordion Widget

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -4,19 +4,19 @@ const Accordion = ({ items }) => {
   const [activeIndex, setActiveIndex] = useState(null);
 
   const onTitleClick = (index) => {
-    setActiveIndex({ activeIndex: index });
-    console.log(activeIndex);
+    setActiveIndex(index);
   };
 
   const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? 'active' : '';
     return (
       <React.Fragment key={item.title}>
-        <div onClick={() => onTitleClick(index)} className='title active'>
+        <div onClick={() => onTitleClick(index)} className={`title ${active}`}>
           <i className='dropdown icon'> </i>
           {item.title}
-          <div className='content active'>
-            <p>{item.content}</p>
-          </div>
+        </div>
+        <div className={`content ${active}`}>
+          <p>{item.content}</p>
         </div>
       </React.Fragment>
     );
